9bcd88db5fa867b25cccb11abd1fadda1209f818
julien <julienremy@hotmail.com>
2016-03-15 19:57:38 +0100

    init

---
+++ src/rmi/Game.java
@@ -0,0 +1,13 @@
+package rmi;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Game extends UnicastRemoteObject{
+
+	protected Game() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}

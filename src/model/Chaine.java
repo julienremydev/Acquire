1532b323ca5c8472d6e6b5314fbb529068bd6c36
Yodaii <yohann.vaubourg@gmail.com>
2016-03-18 11:57:28 +0100

    [Release Model] Chaine + Type Chaine

--- src/model/Chaine.java
+++ src/model/Chaine.java
@@ -1,5 +1,88 @@
 package model;
 
-public class Chaine {
+import java.util.ArrayList;
+import javafx.scene.paint.Color;
 
+public class Chaine {
+	private String nomChaine;
+	private ArrayList<Case> listeCase;
+	private static final int nbActionTotal = 25;
+	private int nbActionRestante;
+	
+	/**
+	 * Constructeur permettant de definir le nom des chaines d hotels
+	 * initialisation des autres attributs a leur valeur par defaut
+	 * @param nom 
+	 */
+	public Chaine(String nom){
+		this.nomChaine = nom;
+		this.nbActionRestante = 25;
+		this.listeCase = new ArrayList<Case>();
+	}
+	
+	/**
+	 * fonction permettant d ajouter une case a la liste de case de la chaine
+	 * @param c
+	 */
+	public void addCase(Case c){
+		this.getListeCase().add(c);
+	}
+
+	/**
+	 * fonction permettant au joueur d acheter des actions et met a jour le nombre d action restante
+	 * @param nb : nombre d action voulant être acheter par le joueur
+	 * @return nombre effectivement acheter
+	 */
+	public int achatActionJoueur(int nb){
+		int res = nb;
+		
+		if(this.getNbActionRestante()-nb < 0){ // on ne peut pas avoir un nombre daction restante negatif
+			this.setNbActionRestante(0);
+			res = this.getNbActionRestante();
+		} else {
+			this.setNbActionRestante(this.getNbActionRestante()-nb);
+		}
+		
+		return res;
+	}
+	
+	/**
+	 * fonction retournant la taille de la chaine d hotel
+	 * @return taille de la chaine dhotel
+	 */
+	public int tailleChaine(){
+		return this.getListeCase().size();
+	}
+	
+	
+	/*
+	 * Liste des Getters et Setters des tous les attributs
+	 */
+	public String getNomChaine() {
+		return nomChaine;
+	}
+
+	public void setNomChaine(String nomChaine) {
+		this.nomChaine = nomChaine;
+	}
+
+	public ArrayList<Case> getListeCase() {
+		return listeCase;
+	}
+
+	public void setListeCase(ArrayList<Case> listeCase) {
+		this.listeCase = listeCase;
+	}
+
+	public int getNbActionRestante() {
+		return nbActionRestante;
+	}
+
+	public void setNbActionRestante(int nbActionRestante) {
+		this.nbActionRestante = nbActionRestante;
+	}
+
+	public static int getNbactiontotal() {
+		return nbActionTotal;
+	}
 }
---
+++ src/model/TypeChaine.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.util.HashMap;
+
+import javafx.scene.paint.Color;
+
+public enum TypeChaine {
+	SACKSON(1, Color.web("#CC3333")),
+	ZETA(1, Color.web("#FFCC33")),
+	HYDRA(2, Color.web("#FF6600")),
+	FUSION(2, Color.web("#336633")),
+	AMERICA(2, Color.web("#333399")),
+	PHOENIX(3, Color.web("#996699")),
+	QUANTUM(3, Color.web("#669999"));
+
+	private int catChaine; // categorie de la chaine (1 = faible, 2 = moyen, 3 = fort)
+	private Color couleurChaine;
+	private static int[] nbHotel = {2,3,4,5,10,20,30,40};
+	
+	private TypeChaine(int cat, Color c){
+		this.catChaine = cat;
+	}
+	
+	private static int prixAction(TypeChaine tc, int nb){
+		int res = 200 + (tc.catChaine - 1) * 100; // prix de base de la chaine d hotel
+		for(int i : nbHotel){
+			if(nb > i){
+				res += 100;
+			} 
+		}	
+		return res;
+	}
+	
+	public static int primeActionnairePrincipal(TypeChaine tc, int nb){
+		return prixAction(tc, nb)*10;
+	}
+	
+	public static int primeActionnaireSecondaire(TypeChaine tc, int nb){
+		return prixAction(tc, nb)*5;
+	}
+}

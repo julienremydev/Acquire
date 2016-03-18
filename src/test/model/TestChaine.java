81606c06e0c9234f8cab281d39a56519103b40d3
Yodaii <yohann.vaubourg@gmail.com>
2016-03-18 15:55:11 +0100

    Test [TestCase TestChaine] implemantation de quelques tests

--- src/test/model/TestChaine.java
+++ src/test/model/TestChaine.java
@@ -2,13 +2,37 @@ package test.model;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import application.model.Chaine;
+
 public class TestChaine {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	Chaine chaine;
+	
+	@Before
+	public void initChaine(){
+		chaine = new Chaine("SACKSON");
 	}
-
+	
+	@Test
+	public void testAchatActionJoueur() {
+		// cas action achete < 0
+		int nbActionAchete = -10;
+		int actionAchete = chaine.achatActionJoueur(nbActionAchete);
+		assertEquals(0, actionAchete);
+		assertEquals(25, chaine.getNbActionRestante());
+		
+		// cas normal action achete < action restantes
+		nbActionAchete = 3;
+		actionAchete = chaine.achatActionJoueur(nbActionAchete);
+		assertEquals(3, actionAchete);
+		assertEquals(22, chaine.getNbActionRestante());
+		
+		// cas action achete > action restantes	
+		nbActionAchete = 24;
+		actionAchete = chaine.achatActionJoueur(nbActionAchete);
+		assertEquals(22, actionAchete);
+		assertEquals(0, chaine.getNbActionRestante());
+	}
 }

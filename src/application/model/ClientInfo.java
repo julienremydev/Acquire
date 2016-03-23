4d7a312d0f3ca43502e24447e623ac3906f88dde
Yodaii <yohann.vaubourg@gmail.com>
2016-03-23 15:46:44 +0100

    Release [ClientInfo] creation de la classe

--- src/application/model/ClientInfo.java
+++ src/application/model/ClientInfo.java
@@ -1,5 +1,57 @@
 package application.model;
 
-public class ClientInfo {
+import java.util.ArrayList;
 
+public class ClientInfo {
+	private String pseudo;
+	private ArrayList<Case> main;
+	private int net;
+	private int cash;
+	
+	/**
+	 * Constructeur de la classe client info qui initialise les donnees d un client
+	 * @param n : nom du pseudo du client
+	 */
+	public ClientInfo(String n){
+		this.pseudo = n;
+		this.net = 6000;
+		this.cash = 6000;
+		this.main = new ArrayList<Case>();
+	}
+
+	
+	/*
+	 * Getter & Setter
+	 */
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public ArrayList<Case> getMain() {
+		return main;
+	}
+
+	public void setMain(ArrayList<Case> main) {
+		this.main = main;
+	}
+
+	public int getNet() {
+		return net;
+	}
+
+	public void setNet(int net) {
+		this.net = net;
+	}
+
+	public int getCash() {
+		return cash;
+	}
+
+	public void setCash(int cash) {
+		this.cash = cash;
+	}
 }

cd42fe216fa4d57ad1cfcd6118c09f207bdbfb52
neo <arthurdu88@hotmail.com>
2016-03-25 15:42:12 +0100

    test + refactor [TableauDeBord] cration des test et ajout des commentaires

--- src/application/model/TableauDeBord.java
+++ src/application/model/TableauDeBord.java
@@ -8,26 +8,53 @@ public class TableauDeBord {
 	private ArrayList<ClientInfo> infoParClient;
 	private ArrayList<Chaine> listeChaine;
 
+	/**
+	 * Constructeur Tableau de bord
+	 */
 	public TableauDeBord() {
 		this.infoParClient = new ArrayList<ClientInfo>();
 		this.listeChaine=new ArrayList<Chaine>();
 	}
+	/**
+	 * Retourne la liste des clients
+	 * @return
+	 */
 	public ArrayList<ClientInfo> getInfoParClient() {
 		return infoParClient;
 	}
+	/**
+	 * Défini la nouvelle liste d'infoParClient avec la lister en paramètre
+	 * @param infoParClient
+	 */
 	public void setInfoParClient(ArrayList<ClientInfo> infoParClient) {
 		this.infoParClient = infoParClient;
 	}
+	/**
+	 * Retourne la liste des chaine
+	 * @return
+	 */
 	public ArrayList<Chaine> getListeTypeChaine() {
 		return listeChaine;
 	}
+	/**
+	 * Permet de définir la liste de chaines
+	 * @param listeTypeChaine
+	 */
 	public void setListeTypeChaine(ArrayList<Chaine> listeTypeChaine) {
 		this.listeChaine = listeTypeChaine;
 	}
+	/**
+	 * Ajout d'un client dans la liste de client
+	 * @param c
+	 */
 	public void ajouterClient(ClientInfo c) {
 		this.infoParClient.add(c);
 	}
-	
+	/**
+	 * Retourne la chaine si on passe correspondante à son id
+	 * @param id
+	 * @return
+	 */
 	public Chaine getChaineById(int id){
 		Chaine toReturn = null;
 		for(Chaine c:listeChaine){
--- src/test/model/TestTableauDeBord.java
+++ src/test/model/TestTableauDeBord.java
@@ -2,13 +2,54 @@ package test.model;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 
-public class TestTableauDeBord {
+import application.model.Chaine;
+import application.model.ClientInfo;
+import application.model.TableauDeBord;
+import application.model.TypeChaine;
 
+public class TestTableauDeBord {
+	ClientInfo c1;
+	ClientInfo c2;
+	Chaine ch1;
+	Chaine ch2;
+	
+	TableauDeBord tableauTest;
+	ArrayList<Chaine> listeTypeChaine;
+	ArrayList<ClientInfo> infoParClient;
+	
+	
+	@Before
+	public void initTableauBord(){
+		tableauTest = new TableauDeBord();
+		infoParClient = new ArrayList<ClientInfo>();
+		listeTypeChaine = new ArrayList<Chaine>();
+//		c1 = new ClientInfo("client1");
+//		c2 = new ClientInfo("client2");
+		ch1 = new Chaine (TypeChaine.AMERICA);
+		ch2 = new Chaine (TypeChaine.FUSION);
+//		infoParClient.add(c1);
+//		infoParClient.add(c2);
+		listeTypeChaine.add(ch1);
+		listeTypeChaine.add(ch2);
+		
+		
+		tableauTest.setListeTypeChaine(listeTypeChaine);
+		tableauTest.setInfoParClient(infoParClient);
+		
+		
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetChaineById() {
+		
+		assertEquals(ch1, tableauTest.getChaineById(6));
+		assertEquals(ch2, tableauTest.getChaineById(5));
+		assertNotEquals(ch2, tableauTest.getChaineById(6));
 	}
 
 }

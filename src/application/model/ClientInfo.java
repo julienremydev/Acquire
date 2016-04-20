cdf8711ca074956582150328922b92fc7459e043
Yodaii <yohann.vaubourg@gmail.com>
2016-04-20 19:20:17 +0200

    Features [TableauDeBord] change organisation

--- src/application/model/ClientInfo.java
+++ src/application/model/ClientInfo.java
@@ -16,6 +16,8 @@ public class ClientInfo implements Serializable{
 	
 	private Integer cash;
 	
+	private HashMap<TypeChaine, Integer> actionParChaine;
+
 	/**
 	 * Constructeur de la classe client info qui initialise les donnees d un client
 	 * @param n : nom du pseudo du client
@@ -25,6 +27,7 @@ public class ClientInfo implements Serializable{
 		this.net = 6000;
 		this.cash = 6000;
 		this.main = new HashMap<String,Case>();
+		this.actionParChaine = new HashMap<TypeChaine,Integer>();
 	}
 
 	/**
@@ -109,6 +112,14 @@ public class ClientInfo implements Serializable{
 		this.cash = cash;
 	}
 
+	public HashMap<TypeChaine, Integer> getActionParChaine() {
+		return actionParChaine;
+	}
+
+	public void setActionParChaine(HashMap<TypeChaine, Integer> actionParChaine) {
+		this.actionParChaine = actionParChaine;
+	}
+	
 	public String getPseudo() {
 		// TODO Auto-generated method stub
 		return pseudo;
--- src/application/model/TableauDeBord.java
+++ src/application/model/TableauDeBord.java
@@ -69,6 +69,110 @@ public class TableauDeBord implements Serializable{
 		}
 		return toReturn;
 	}
+	
+	/**
+	 * fonction permettant au joueur d acheter des actions et met a jour le nombre d action restante
+	 * @param nb : nombre d action voulant etre acheter par le joueur
+	 * @param nomJoueur : nom du joueur qui achete
+	 * @param tc : type chaine de la chaine
+	 * @return nombre effectivement acheter
+	 */
+	public int achatActionJoueur(int nb, String nomJoueur, TypeChaine tc){
+		int indexChaine = -1;
+		int indexJoueur = -1;
+		
+		for(int i=0; i<listeChaine.size(); i++){
+			if (listeChaine.get(i).getNomChaine().equals(tc)){
+				indexChaine = i;
+			}
+		}
+		for(int i=0; i<infoParClient.size(); i++){
+			if (infoParClient.get(i).getPseudo() == nomJoueur){
+				indexJoueur = i;
+			}
+		}
+		int res = 0;
+		
+		if (indexChaine != -1 && indexJoueur != -1){
+			if(nb < 0 || this.listeChaine.get(indexChaine).getNbActionRestante() == 0){
+				nb = 0;
+			}
+			
+			if(this.listeChaine.get(indexChaine).getNbActionRestante()-nb < 0){ // on ne peut pas avoir un nombre daction restante negatif
+				res = this.listeChaine.get(indexChaine).getNbActionRestante();
+				this.listeChaine.get(indexChaine).setNbActionRestante(0);
+			} else {
+				res = nb;
+				this.listeChaine.get(indexChaine).setNbActionRestante(this.listeChaine.get(indexChaine).getNbActionRestante()-nb);
+			}
+			
+			if(res != 0){
+				if(this.infoParClient.get(indexJoueur).getActionParChaine().containsKey(tc)){
+					this.infoParClient.get(indexJoueur).getActionParChaine().put(tc, res + this.infoParClient.get(indexJoueur).getActionParChaine().get(tc));
+				}else{
+					this.infoParClient.get(indexJoueur).getActionParChaine().put(tc, res);			
+				}			
+			}
+		}
+		
+		return res;
+	}
+	
+	/**
+	 * fonction permettant au joueur de vendre des actions et met a jour le nombre d action restante
+	 * @param nb : nombre d action voulant etre vendue par le joueur
+	 * @param nomJoueur : nom du joueur qui vend
+	 * @param tc : type de la chaine
+	 * @return nombre effectivement vendue
+	 */
+	public int vendActionJoueur(int nb, String nomJoueur, TypeChaine tc){
+		int indexChaine = -1;
+		int indexJoueur = -1;
+		
+		for(int i=0; i<listeChaine.size(); i++){
+			if (listeChaine.get(i).getNomChaine().equals(tc)){
+				indexChaine = i;
+			}
+		}
+		for(int i=0; i<infoParClient.size(); i++){
+			if (infoParClient.get(i).getPseudo() == nomJoueur){
+				indexJoueur = i;
+			}
+		}
+		
+		int res = 0;
+		
+		if (indexChaine != -1 && indexJoueur != -1){
+			boolean joueurExiste = this.infoParClient.get(indexJoueur).getActionParChaine().containsKey(tc);
+			if (nb < 0 || !joueurExiste){
+				nb = 0;
+			}
+			
+			if (joueurExiste && nb > this.infoParClient.get(indexJoueur).getActionParChaine().get(tc)){
+				res = this.infoParClient.get(indexJoueur).getActionParChaine().get(tc);
+			}
+			
+			if(this.listeChaine.get(indexChaine).getNbActionRestante()+nb > Chaine.getNbactiontotal()){ // on ne peut pas avoir plus de 25 action
+				res = Chaine.getNbactiontotal()-this.listeChaine.get(indexChaine).getNbActionRestante();
+				this.listeChaine.get(indexChaine).setNbActionRestante(Chaine.getNbactiontotal());
+			} else {
+				res=nb;
+				this.listeChaine.get(indexChaine).setNbActionRestante(this.listeChaine.get(indexChaine).getNbActionRestante()+nb);
+			}
+			
+			if(joueurExiste){
+				if(this.infoParClient.get(indexJoueur).getActionParChaine().get(tc) - res > 0){
+					this.infoParClient.get(indexJoueur).getActionParChaine().put(tc, this.infoParClient.get(indexJoueur).getActionParChaine().get(tc) - res);
+					
+				}else{
+					this.infoParClient.get(indexJoueur).getActionParChaine().remove(tc);
+				}
+			}
+		}
+		
+		return res;
+	}
+	
 	/**
 	 * Creation d'une nouvelle chaine, Changement de l'etat des hotels en chaîne, ajout de la chaine, à la liste de chaîne.
 	 * @param listeHotels

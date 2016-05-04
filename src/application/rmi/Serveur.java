50c2471562175bcc38266fc45e1776e5cb735179
julien <julienremy@hotmail.com>
2016-05-04 22:08:57 +0200

    Case Noir Refresh

--- src/application/rmi/Serveur.java
+++ src/application/rmi/Serveur.java
@@ -295,9 +295,7 @@ public class Serveur extends UnicastRemoteObject implements ServeurInterface {
 		HashMap<String,String> listeCasesNoires = new HashMap<String,String>();
 		while (enumKeys.hasMoreElements()) {
 			String key = enumKeys.nextElement();
-			for (int i = 0; i<30;i++) {
-				listeCasesNoires.put(key,game.getPlateau().initialiseMainCaseNoir());
-			}
+			listeCasesNoires.put(key,game.getPlateau().initialiseMainCaseNoir());
 			game.getTableauDeBord().getInfoParClient().get(key).initialiseMain(game.getPlateau());
 
 		}

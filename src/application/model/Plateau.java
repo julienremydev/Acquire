c7be0662fb013ce045ed030cf507221c382721b7
neo <arthurdu88@hotmail.com>
2016-05-05 17:52:12 +0200

    testing[plateau] fusion de fonctions

--- src/application/model/Plateau.java
+++ src/application/model/Plateau.java
@@ -250,58 +250,45 @@ public class Plateau implements Serializable {
 
 	private Action gestionOnlyChain(ArrayList<Chaine> listeChaine, Case caseModifiee) {
 		ArrayList<Case> tab = caseModifiee.tabAdjascent();
-		/*
-		 * Le tableau n'a qu'une taille de 1, donc simple changement de la couleur de la case
-		 */
-		if (tab.size() == 1)
-		{
-			// La liste de chaine va de 0 a 7 alors que nos états vont de 2 a 8, le -2 est alors nécessaire
-			// addCase ajoute la case dans la chaine, et change par la même occasion l'état de la case
-			listeChaine.get(tab.get(0).getEtat()-2).addCase(caseModifiee);
+
+		ArrayList<Chaine> chaineDifferente = listeChaineDifferentes(tab, listeChaine);
+		ArrayList<Chaine> listeGrandesChaines = new ArrayList<>();
+		//Creation de la liste de Case hotels éventuelles à intégrer dans la fusion
+		Set<Case> setCasesAModifier = new HashSet<Case>();
+		setCasesAModifier.addAll(addRecurse(setCasesAModifier,caseModifiee));
+		ArrayList<Case> tabCasesAModifier = new ArrayList<Case> (setCasesAModifier);
+
+		if(chaineDifferente.size()==1)
+			//Le nombre de chaine différente est de 1, donc la case est entourée par la même chaine, donc la grande chaine est la seule chaine
+			listeGrandesChaines=chaineDifferente;
+		else
+			//Nous avons plusieurs chaines différentes, on regarde si elles sont toutes la même taille
+			listeGrandesChaines = sameSizeChaine(chaineDifferente);
+
+		// Si les chaines ont toutes la même taille, listeGrandeChaine est a null
+		if(listeGrandesChaines == null){
+
+			return new Action(Globals.choixActionFusionSameSizeChaine,listeChaine,chaineDifferente, chaineDifferente,tabCasesAModifier);
 		}
-		/*
-		 * Tableau taille de >2, donc plusieures cases avec une chaine
-		 */
-		if (tab.size() >= 2){
-			boolean sameColor = caseModifiee.sameColorsArround(tab);				
-			// de la même couleur donc on ajoute simplement la case à la chaine
-			if (sameColor)
-				listeChaine.get(tab.get(0).getEtat()-2).addCase(caseModifiee);
-			else{
-				// On vérifie la taille des chianes pour savoir si elle sont différentes. 
-				ArrayList<Chaine> chaineDifferente = listeChaineDifferentes(tab, listeChaine);
-				ArrayList<Chaine> listeGrandeChaine = new ArrayList<>();
-				listeGrandeChaine = sameSizeChaine(chaineDifferente);
-
-				Set<Case> setCasesAModifier = new HashSet<Case>();
-				ArrayList<Case> tabCasesAModifier = new ArrayList<Case> ();
-				setCasesAModifier.addAll(addRecurse(setCasesAModifier,caseModifiee));
-				tabCasesAModifier.add(caseModifiee);
-
-				// Toutes les chaines de la même taille
-				if(listeGrandeChaine == null){
-					return new Action(Globals.choixActionFusionSameSizeChaine,listeChaine,chaineDifferente, chaineDifferente,tabCasesAModifier);
-				}
-				else{
-					if(listeGrandeChaine.size()==1)
-					{
-						ArrayList<Chaine> listeChaineDifferenteAvantModif = chaineDifferente;
-						Chaine chaineAbsorbanteAvantFusion = listeGrandeChaine.get(0);
-						Chaine grandeChaineAction = listeGrandeChaine.get(0);
-						ArrayList<Case> listeCaseAbsorbee1 = new ArrayList<Case>();
-						listeCaseAbsorbee1.add(caseModifiee);
-						fusionChaines(listeChaine,chaineDifferente,grandeChaineAction,tabCasesAModifier);
-						return new Action(Globals.choixActionFusionEchangeAchatVente,listeChaineDifferenteAvantModif,chaineAbsorbanteAvantFusion);
-					}
-					else
-					{
-						return new Action(Globals.choixActionFusionSameSizeChaine,listeChaine,listeGrandeChaine, chaineDifferente,tabCasesAModifier);
-					}
-
-				}
-
-
+		// Nous avons au moins une chaine plus grande qu'une autre
+		else{
+			// Si la taille est de 1, nous n'avons qu'une grande chaine, donc on fais la modification puis return l'Action pour les échanges d'action
+			if(listeGrandesChaines.size()==1)
+			{
+				// Sauvegarde de l'état des chaines avant fusion pour l'échange d'actions
+				ArrayList<Chaine> listeChaineDifferenteAvantModif = chaineDifferente;
+				Chaine chaineAbsorbanteAvantFusion = listeGrandesChaines.get(0);
+				Chaine chaineAbsorbantePourFusion = listeGrandesChaines.get(0);
+				// Fusion des Chaines
+				fusionChaines(listeChaine,chaineDifferente,chaineAbsorbantePourFusion,tabCasesAModifier);
+				// Puis création de l'action avec les chaines précédant leur modification			
+				return new Action(Globals.choixActionFusionEchangeAchatVente,listeChaineDifferenteAvantModif,chaineAbsorbanteAvantFusion);
 			}
+			// Sinon 2 chaines de même taille sont plus grande qu'une autre, on return donc l'Action pour le choix de la chaine Absorbante
+			else
+			{
+				return new Action(Globals.choixActionFusionSameSizeChaine,listeChaine,listeGrandesChaines, chaineDifferente,tabCasesAModifier);
+			}		
 		}
 		return null;
 	}

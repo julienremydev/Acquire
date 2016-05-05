75330d46fa49fe1569c6e225c3249d0a435be110
Yodaii <yohann.vaubourg@gmail.com>
2016-05-05 03:58:10 +0200

    Bug Fix

--- src/application/rmi/Serveur.java
+++ src/application/rmi/Serveur.java
@@ -351,7 +351,6 @@ public class Serveur extends UnicastRemoteObject implements ServeurInterface {
 		HashMap<String,String> listeCasesNoires = new HashMap<String,String>();
 		while (enumKeys.hasMoreElements()) {
 			String key = enumKeys.nextElement();
-			for (int i = 0;i<20 ; i++)
 			listeCasesNoires.put(key,game.getPlateau().initialiseMainCaseNoir(false));
 			game.getTableauDeBord().getInfoParClient().get(key).initialiseMain(game.getPlateau());
 
